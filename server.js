074a3a582e288abd6c9c0fc8da0814f0f7afc3d3
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2024-12-10 06:56:46 +0530

    Final Commit

--- server.js
+++ server.js
@@ -169,6 +169,12 @@ app.get("/", (req, res) => {
   res.status(200).send("Welcome to Certificate Verification Server");
 });
 
+// Poling the server for activeness
+app.get("/api/ping", (req, res) => {
+  console.info("Server is alive!");
+  res.status(200).send("Server is alive!");
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
